8be8e8f6279167a945d8481ce1640e6a168d32e4
bugrahanozturkk <70200551+bugrahanozturkk@users.noreply.github.com>
2022-01-28 14:43:18 +0300

    Add the bot intents and create the  bots tasks

--- bot.js
+++ bot.js
@@ -2,18 +2,60 @@ const Redis = require("ioredis");
 const { Client, Intents } = require("discord.js");
 
 require("dotenv").config();
-const SECRET_WORD = "hack";
+const SECRET_WORD = "fight";
 const SECRET_COUNT = 3;
-const SECRET_ROLE = "The Anonymous";
+const SECRET_ROLE = "Fight Club";
 
 const redis = new Redis(process.env.REDIS_URL);
 
 //Create a new Discord client
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+  intents: [
+    "GUILDS",
+    "GUILD_MESSAGES",
+    "GUILD_MEMBERS",
+    "GUILD_MESSAGE_REACTIONS",
+    "DIRECT_MESSAGES",
+    "GUILD_MESSAGE_REACTIONS",
+    "DIRECT_MESSAGE_REACTIONS",
+  ],
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
+});
 client.login(process.env.DISCORD_TOKEN);
 
 client.on("ready", () => {
-  client.on("message", (message) => {
-    console.log(message);
-  });
+  console.log("ready");
+});
+
+client.on("messageCreate", async (messageCreate) => {
+  if (messageCreate.author.bot) return;
+  if (messageCreate.channel.type === "DM") return;
+  let keyspace = `guild:${messageCreate.guild.id}:user:${messageCreate.author.id}`;
+  let countKey = `${keyspace}:count`;
+  let confirmedKey = `${keyspace}:confirm`;
+  let filterKey = `${keyspace}:filter`;
+
+  let confirmed = await redis.getbit(confirmedKey, 0);
+  if (confirmed) return;
+
+  let count = messageCreate.content
+    .split(" ")
+    .map((word) => word.toLowerCase())
+    .filter((word) => word === SECRET_WORD).length;
+
+  if (!count) return;
+
+  let totalCount = await redis.incrby(countKey, count);
+  if (totalCount >= SECRET_COUNT) {
+    let role = messageCreate.guild.roles.cache.find(
+      (role) => role.name === SECRET_ROLE
+    );
+    if (role) {
+      messageCreate.member.roles.add(role);
+      redis.setbit(confirmedKey, 0, 1);
+      messageCreate.author.send(
+        "Welcome to Fight Club 💪. The first rule of Fight Club is: you do not talk about Fight Club."
+      );
+    }
+  }
 });
